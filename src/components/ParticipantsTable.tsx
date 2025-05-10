d9d105415c84e5cf91986d5bd25c78ed43d6cd77
Administrator <victor@fluux.eu>
2025-05-10 21:11:18 +0300

    0.0.2

--- src/components/ParticipantsTable.tsx
+++ src/components/ParticipantsTable.tsx
@@ -26,19 +26,32 @@ const ParticipantsTable = () => {
     const [participants, setParticipants] = useState<Participant[]>([]);
     const [checkedParticipants, setCheckedParticipants] = useState<number[]>([]);
 
-    useEffect(() => {
+    const getParticipants = () => {
         axios.get<Participant[]>('http://localhost:5100/api/v1/participants', {
             headers: {
                 'Content-Type': 'application/json'
             }
         })
             .then((response) => {
+                if (response.data.status !== 'success') {
+                    console.error('Error fetching participants:', response.data.message);
+                    return;
+                }
                 setParticipants(response.data.data);
+                console.log("Checked participants:");
                 // console.log(participants);
             })
             .catch((error) => {
                 console.error('Error fetching participants:', error);
             });
+    }
+
+    useEffect(() => {
+        // Fetch participants when the component mounts and every 5 seconds
+        getParticipants();
+        setInterval(() => {
+            getParticipants();
+        }, 5000);
     }, []);
 
     useEffect(() => {
@@ -65,6 +78,23 @@ const ParticipantsTable = () => {
         });
     };
 
+    const handleDelete = () => {
+        checkedParticipants.forEach((id) => {
+            axios.delete(`http://localhost:5100/api/v1/participants/${id}`, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(() => {
+                    setParticipants((prev) => prev.filter((participant) => participant.id !== id));
+                    setCheckedParticipants((prev) => prev.filter((participantId) => participantId !== id));
+                })
+                .catch((error) => {
+                    console.error('Error deleting participant:', error);
+                });
+        });
+    }
+
 
     return (
         <div className="table-ctn">
@@ -79,27 +109,43 @@ const ParticipantsTable = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {participants.map((participant, index) => (
-                        <tr key={index}>
-                            <td className='checkbox-ctn'>
-                                <label htmlFor={"for_deletion-" + participant.id} >
-                                    <input id={"for_deletion-" + participant.id} type="checkbox" onClick={handleCheckboxChange(participant.id)} />
-                                </label>
-                            </td>
-                            <td>{participant.name}</td>
-                            <td>{participant.email}</td>
-                            <td>{participant.birthdate} ({getAge(participant.birthdate)})</td>
+                    {participants.length > 0 ? (
+                        participants.map((participant, index) => (
+                            <tr key={index} className="table-row">
+                                <td className='checkbox-ctn'>
+                                    <label htmlFor={"for_deletion-" + participant.id} >
+                                        <input id={"for_deletion-" + participant.id} type="checkbox" onClick={handleCheckboxChange(participant.id)} />
+                                    </label>
+                                </td>
+                                <td>{participant.name}</td>
+                                <td>{participant.email}</td>
+                                <td>{participant.birthdate} ({getAge(participant.birthdate)})</td>
 
-                            <td>
-                                <button type='submit' className='edit-button'>
-                                    <span className='edit-icon'></span>
-                                </button>
+                                <td>
+                                    <button type='submit' className='edit-button'>
+                                        <span className='edit-icon'></span>
+                                    </button>
+                                </td>
+                            </tr>
+                        ))
+                    ) : (
+                        <tr>
+                            <td className="no-data">
+                                Empty
                             </td>
                         </tr>
-                    ))}
+                    )}
 
                 </tbody>
             </table >
+            <div className="btn-row">
+                <button className="add-btn">
+                    Add Participant
+                </button>
+                <button onClick={handleDelete} className="remove-btn">
+                    Remove Participant
+                </button>
+            </div>
         </div >
     )
 }
